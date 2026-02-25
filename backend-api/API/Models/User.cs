08b9e64c09bcc16ac4748036fd4452ec1558972e
Uchechukwu Onwe <maxoutguide24@gmail.com>
2026-02-25 12:56:22 -0600

    Backend architecture set up (Models, Data, removed template files)

---
+++ backend-api/API/Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace API.Models;
+
+public class User
+{
+    public int id {get; set;}
+    public string FullName { get; set;} = string.Empty;
+
+    public string Email { get; set;} = string.Empty;
+
+    public string PasswordHash { get; set;} = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+}
--- backend-api/API/Program.cs
+++ backend-api/API/Program.cs
@@ -1,12 +1,29 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
+// Add DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 // Configure the HTTP request pipeline. (middleware)
+app.UseHttpsRedirection();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
