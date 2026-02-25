021d39b9b9caa8b3cff3bb2253192df5d4475204
J0-0L26 <146900993+J0-0L26@users.noreply.github.com>
2026-02-25 16:36:22 -0600

    updates needed for me to run expo and attempting to add a new tab

--- mobile-app/app/(tabs)/_layout.tsx
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -30,6 +30,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name = "new"
+        options={{
+          title: 'New',
+          tabBarIcon: ({ color }) => <IconSymbol size ={28} name="paperplane.fill" color = {color} />,
+        }}
+      />
     </Tabs>
   );
 }
